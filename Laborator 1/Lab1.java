0460bb5f2f23c64f99486d2222cf8b908d1484fc
EnascutaRazvan <enascutar@gmail.com>
2022-02-23 00:29:10 +0200

    Laborator 1

--- Laborator 1/Lab1.java
+++ Laborator 1/Lab1.java
@@ -1,27 +1,33 @@
 package lab1;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-
 public class Lab1 {
     public static void main(String[] args) {
-	
+
         Lab1 lab1 = new Lab1();
         lab1.compulsory();
     }
 
-    void compulsory(){
+    void compulsory() {
+
         System.out.println("Hello World");
         String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
-        int n = (int) (Math.random() * 1_000_000);
-		n = n * 3;
+
+        int n = (int) (Math.random() * 1000000);
+        n = n * 3;
         n = n + 0b10101;
         n = n + 0xFF;
         n = n * 6;
-        int result = 0;
-        while(n>=10)
-            n=n/10+n%10;
+        int result;
+        int Ultima_cifra;
+
+        System.out.println(n);
+        while (n >= 10){
+            Ultima_cifra = n % 10;
+            n = n / 10 + Ultima_cifra;
+        }
+
+
         result = n;
         System.out.println("\nWilly-nilly, this semester I will learn " + languages[result]);
     }
+}
